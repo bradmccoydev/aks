9d6b07f37cc4d903ba5ad1e3ded66a9c82dd8e75
Ben Poh <benhpoh@gmail.com>
2022-01-25 11:58:19 +1100

    add kubernetes secret for argo
    Signed-off-by: Ben Poh <benhpoh@gmail.com>

--- src/provider.tf
+++ src/provider.tf
@@ -9,10 +9,19 @@ terraform {
 
 provider "azurerm" {
   features {}
-  partner_id = "d1e4547f-b660-46e1-a8fd-0189ab5ced8f"
+  partner_id                   = "d1e4547f-b660-46e1-a8fd-0189ab5ced8f"
   disable_terraform_partner_id = true
-  skip_provider_registration = true
+  skip_provider_registration   = true
 }
 
 provider "random" {
+}
+
+provider "kubernetes" {
+  host                   = module.azurerm_kubernetes_cluster_primary.kube_config.0.host
+  username               = module.azurerm_kubernetes_cluster_primary.kube_config.0.username
+  password               = module.azurerm_kubernetes_cluster_primary.kube_config.0.password
+  client_certificate     = base64decode(module.azurerm_kubernetes_cluster_primary.kube_config.0.client_certificate)
+  client_key             = base64decode(module.azurerm_kubernetes_cluster_primary.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(module.azurerm_kubernetes_cluster_primary.kube_config.0.cluster_ca_certificate)
 }
