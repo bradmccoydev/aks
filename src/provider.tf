0d513c70ff84c414e20b0499f0be9380c2e54c2e
Brad McCoy <brad.mccoy@basiq.io>
2022-06-10 09:20:30 +1000

    update config
    Signed-off-by: Brad McCoy <brad.mccoy@basiq.io>

--- src/provider.tf
+++ src/provider.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "devops-prod"
-    storage_account_name = "moulainfrastate"
-    container_name       = "moula-training"
-    key                  = "hug-lab.tfstate"
+    storage_account_name = "orteliusinfrastate"
+    container_name       = "prod"
+    key                  = "prod.tfstate"
   }
 }
 
@@ -15,13 +15,4 @@ provider "azurerm" {
 }
 
 provider "random" {
-}
-
-provider "kubernetes" {
-  host                   = module.azurerm_kubernetes_cluster_primary.kube_config.0.host
-  username               = module.azurerm_kubernetes_cluster_primary.kube_config.0.username
-  password               = module.azurerm_kubernetes_cluster_primary.kube_config.0.password
-  client_certificate     = base64decode(module.azurerm_kubernetes_cluster_primary.kube_config.0.client_certificate)
-  client_key             = base64decode(module.azurerm_kubernetes_cluster_primary.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(module.azurerm_kubernetes_cluster_primary.kube_config.0.cluster_ca_certificate)
 }
